314660f438c9a6d7aa934d59d35350a396341eb7
AzizJon <azizjonatx@gmail.com>
2020-09-20 11:27:04 +0500

    hw 25 version 1.0.3

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ else {
 } */
  
 
-var yosh = prompt ('Yoshingiz nechchida?')
+/* var yosh = prompt ('Yoshingiz nechchida?')
 if (yosh >= 0 && yosh <= 18) {
     alert('Yoshsiz, O\'qishingiz kerak') ;
 }
@@ -19,6 +19,32 @@ else if (yosh >=50 && yosh <=59) {
 }
 else if (yosh >=59 && yosh <= 150)
     alert('Pensionerga o\'xshaysiz, hali tirik bo\'lsangiz :\)')
+else {
+    alert('Nimadur noto\'g\'ri ketdi')
+} */
+
+var firstNumber = prompt('Hohlagan soningizni kiriting');
+var secondNumber = prompt('Hohlagan soningizni kiriting');
+var thirdNumber = prompt('Hohlagan soningizni kiriting');
+
+if (firstNumber > secondNumber && firstNumber < thirdNumber) {
+    alert(firstNumber);
+}
+else if (secondNumber > firstNumber && secondNumber < thirdNumber) {
+    alert(secondNumber);
+}
+else if (thirdNumber > secondNumber && thirdNumber < firstNumber) {
+    alert(thirdNumber);
+}
+else if (firstNumber < secondNumber && firstNumber > thirdNumber) {
+    alert(firstNumber);
+}
+else if (secondNumber < firstNumber && secondNumber > thirdNumber) {
+    alert(secondNumber);
+}
+else if (thirdNumber < secondNumber && thirdNumber > firstNumber) {
+    alert(thirdNumber);
+}
 else {
     alert('Nimadur noto\'g\'ri ketdi')
 }
