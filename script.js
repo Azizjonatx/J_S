6fbac1ac61bee379c8a19ae761c9eedb4af35b75
AzizJon <azizjonatx@gmail.com>
2020-09-20 09:54:39 +0500

    hm23 version 1.0.2

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-ism = prompt('Ismingizni yozing')
+/* ism = prompt('Ismingizni yozing')
 if (ism ==='Azizjon') {
     alert('Salom Boss');
 }
-else if (ism != 'Azizjon') {
+else {
     alert('Salom mehmon');
-}
+} */
  
 
+var yosh = prompt ('Yoshingiz nechchida?')
+if (yosh >= 0 && yosh <= 18) {
+    alert('Yoshsiz, O\'qishingiz kerak') ;
+}
+else if (yosh >=18 && yosh <=50) {
+    alert('Ishlashingiz kerak');
+}
+else if (yosh >=50 && yosh <=59) {
+    alert('Yaqinda pensiyaga chiqasiz');
+}
+else if (yosh >=59 && yosh <= 150)
+    alert('Pensionerga o\'xshaysiz, hali tirik bo\'lsangiz :\)')
+else {
+    alert('Nimadur noto\'g\'ri ketdi')
+}
